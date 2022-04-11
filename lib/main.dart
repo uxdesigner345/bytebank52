2ee479a9d111e4ece37f321ab1c9b5dc43733ea4
Sandro Gomes <sandrodesigner67@gmail.com>
2022-04-11 12:27:52 -0300

    novo commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,57 @@ class BytebankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
+      theme: ThemeData.dark(),
+      home: Dashboard(),
+      //Scaffold
+    ); //MaterialApp
+  }
+}
+class Dashboard extends StatelessWidget {
+  const Dashboard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
           title: Text('Dashboard'),
+
         ),
-      ), //Scaffold
-    ); //MaterialApp
+        body:Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset('images/1c54f7b06d7723c21afc5035bf88a5ef.png'),
+
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                  padding: EdgeInsets.all(10.0),
+                  width: 150,
+                  height: 100,
+                  color: Theme.of(context).primaryColor,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Icon(
+                        Icons.people,
+                        size:28,
+                      ),
+                      Text(
+                          'Contato',
+                          style:TextStyle(
+                            fontSize:25.0,
+                          )
+                      ),
+
+                    ],
+                  )
+              ),
+            )
+          ],
+        )
+    );
   }
 }
 
